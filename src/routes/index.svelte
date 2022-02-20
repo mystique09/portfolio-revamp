2f6739ffad498e0c833c84b6efe0a192056f84af
mystique09 <knightkrypto7@gmail.com>
2022-02-21 03:30:02 +0800

    update homepage design

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import BtnIcon from 'carbon-icons-svelte/lib/Information24';
+</script>
+
+<div class="main">
+	<div class="intro">
+		<p>Hello, I am</p>
+		<h2>Benjie Ben Garcia</h2>
+		<p>A self-taught programmer</p>
+	</div>
+	<div class="img" />
+</div>
+<a class="btn" href="#abot-me">
+	<BtnIcon />
+	<span>About Me</span>
+</a>
+
+<style lang="postcss">
+	.main {
+		@apply p-2 flex flex-col-reverse gap-4;
+	}
+	.intro {
+		@apply flex flex-col gap-2;
+	}
+	p:nth-child(3) {
+		@apply text-blue-400 text-base;
+	}
+	p {
+		@apply font-light text-xl;
+	}
+	.img {
+		@apply h-72 bg-gray-200 max-w-md;
+	}
+	h2 {
+		@apply font-bold text-3xl;
+	}
+	.btn {
+		@apply mt-2 text-gray-200 py-2 text-center flex items-center justify-center gap-4 bg-blue-600 m-auto w-72 h-16 rounded-lg;
+	}
+	.btn span {
+		@apply font-semibold text-lg;
+	}
+</style>
+
