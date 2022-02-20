fdccd44a5a1ca17ee931c756f685e26f71d6b76a
mystique09 <knightkrypto7@gmail.com>
2022-02-21 00:44:02 +0800

    add basic nav structure

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,22 @@
+<div class="nav">
+	<div class="nav-logo">Portfolio</div>
+	<div class="nav-links">
+		<ul>
+			<li><a href="#about-me">About Me</a></li>
+			<li><a href="#projects">Projects</a></li>
+			<li><a href="#skills">Skills</a></li>
+			<li><a href="#contact-me">Contact Me</a></li>
+		</ul>
+	</div>
+	<div class="menu-icon" />
+</div>
+
+<style lang="postcss">
+	.nav {
+		@apply flex flex-row items-center justify-between;
+	}
+
+	.nav-links {
+		@apply hidden md:flex;
+	}
+</style>
