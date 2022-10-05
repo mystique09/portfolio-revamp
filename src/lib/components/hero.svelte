c80d4bf6523a7d359fe22d358dabe814d677f63c
mystique09 <knightkrypto7@gmail.com>
2022-10-05 16:41:46 +0800

    feat: desktop responsive fix: image size

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -2,32 +2,29 @@
 	import BtnIcon from 'carbon-icons-svelte/lib/ArrowDown24';
 </script>
 
-<main id="#" class="dark:text-gray-300 dark:bg-gray-900">
-	<div class="wrap md:flex-row-reverse md:items-center md:justify-center md:m-auto md:max-w-6xl">
+<main id="#" class="md:h-full bg-gray-300">
+	<div class="h-full p-4 flex flex-col-reverse md:flex-row-reverse m-auto md:items-center sm:items-start justify-around max-w-6xl">
 		<div class="intro md:gap-4 md:w-1/2 dark:text-white">
 			<p>Hello, I am</p>
 			<h2>Benjie Ben Garcia</h2>
 			<p class="dark:text-gray-400">A self-taught programmer</p>
 			<p class="dark:text-gray-300 md:text-lg">• I am currently learning Go and Rust.</p>
-			<a class="btn dark:bg-accent dark:text-white" href="#about-me">
+			<a class="btn bg-gray-100 text-black dark:text-white" href="#about-me">
 				<BtnIcon
 					class="transition-300 animate-bounce-alt fw100 animate-count-infinite animate-duration-1s"
 				/>
 				<span>About Me</span>
 			</a>
 		</div>
-		<div class="img md:w-1/2">
-			<img src="./undraw_welcome.svg" alt="Welcome Avatar" />
+		<div class="max-w-2xl md:max-w-md sm:w-1/2 sm:h-1/4 md:w-full md:h-full">
+			<img class="fill-accent" src="./undraw_welcome.svg" alt="Welcome Avatar" />
 		</div>
 	</div>
 </main>
 
 <style>
 	main {
-		@apply bg-gray-300 text-black p-2 py-2 h-screen justify-end gap-4;
-	}
-	.wrap {
-		@apply h-full flex flex-col-reverse;
+		@apply text-black py-4 justify-end gap-4;
 	}
 	.intro {
 		@apply flex flex-col gap-2 items-start justify-evenly;
@@ -41,9 +38,6 @@
 	p {
 		@apply font-light text-xl;
 	}
-	.img {
-		@apply max-w-2xl w-full;
-	}
 	img {
 		@apply w-full h-full;
 	}
@@ -51,9 +45,9 @@
 		@apply font-bold text-3xl;
 	}
 	.btn {
-		@apply shadow-md py-2 text-center flex items-center justify-center gap-2 bg-white text-black m-auto w-56 h-14 rounded-lg mt-4;
+		@apply shadow-md py-2 flex items-center gap-1 justify-center m-auto w-56 h-14 rounded-lg mt-4;
 	}
 	.btn span {
-		@apply font-normal text-sm;
+		@apply font-bold text-sm;
 	}
 </style>
