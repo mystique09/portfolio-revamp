c7e2b432b0cb5d3fcf9d1dc5c1326062fdf401e2
mystique09 <knightkrypto7@gmail.com>
2022-10-05 16:41:56 +0800

    feat: desktop fix: image

--- src/lib/components/about_me.svelte
+++ src/lib/components/about_me.svelte
@@ -1,13 +1,14 @@
 <main
 	id="about-me"
-	class="md:flex-row md:items-center md:m-auto dark:bg-gray-900 md:justify-center"
+	class="flex items-center dark:text-gray-300 bg-gray-300 dark:bg-gray-900"
 >
-	<div class="img md:w-1/2">
+<div class="p-4 flex flex-col md:flex-row m-auto items-center justify-around max-w-6xl gap-2">
+	<div class="max-w-md md:w-full md:h-full sm:h-1/2 sm:w-1/2">
 		<img src="./undraw_programming_re_kg9v.svg" alt="Person avatar" />
 	</div>
 	<div class="content">
 		<h2 class="dark:text-gray-100">About Me</h2>
-		<p class="md:text-base dark:text-gray-300">
+		<p class="md:text-base dark:text-gray-300 max-w-md">
 			I am a self-taught programmer from PH, currently learning Go for the backend and Svelte for
 			the frontend, and I also have an interest in learning Rust.
 		</p>
@@ -16,20 +17,18 @@
 			<a href="https://www.linkedin.com/in/benjie-ben-garcia-916261202">LinkedIn</a>
 		</div>
 	</div>
+</div>
 </main>
 
 <style>
 	main {
-		@apply h-screen pt-12 flex flex-col gap-4 bg-gray-300 text-black px-2 py-12;
-	}
-	.img {
-		@apply mt-16 max-w-2xl;
+		@apply text-black p-2 py-4 h-[100vh] justify-end items-center gap-4;
 	}
 	img {
-		@apply h-full w-full;
+		@apply max-w-md max-h-md h-full w-full;
 	}
 	.content {
-		@apply flex flex-col items-start gap-3 mt-4 max-w-md py-12;
+		@apply flex flex-col items-start gap-3 mt-4 py-12;
 	}
 	.content h2 {
 		@apply font-bold text-gray-900 text-4xl;
