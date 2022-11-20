4f80afa1be488041b5f479be3507d3f1f40e98e4
mystique09 <knightkrypto7@gmail.com>
2022-11-21 03:11:21 +0800

    add: darkmode, and label for link

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -7,11 +7,11 @@
 	$: currentSkill = skills[i];
 </script>
 
-<main id="#" class="hero min-h-screen bg-base-100">
+<main id="#" class="hero min-h-screen bg-base-100 dark:bg-neutral dark:text-neutral-content">
 	<div class="hero-content max-w-4xl w-full flex justify-start">
 		<div class="max-w-lg">
 			<p class="text-xs mt-1 font-thin badge badge-primary dark:badge-secondary">{currentSkill}</p>
-			<h1 class="text-7xl md:text-8xl font-bold text-base-content">Benjie Ben Garcia</h1>
+			<h1 class="text-7xl md:text-8xl font-bold text-base-content dark:text-neutral-content">Benjie Ben Garcia</h1>
 			<p class="mt-2 text-lg md:text-xl">Hey, welcome to my portfolio. Enjoy!</p>
 			<div class="socials">
 			</div>
