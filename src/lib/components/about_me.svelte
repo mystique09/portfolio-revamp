9670599d534994807bec55bd59a32202183f6fd1
mystique09 <knightkrypto7@gmail.com>
2022-11-21 03:10:06 +0800

    add: darkmode, and label

--- src/lib/components/about_me.svelte
+++ src/lib/components/about_me.svelte
@@ -1,14 +1,14 @@
-<div id="about-me" class="about-me hero min-h-screen bg-base-100">
+<div id="about-me" class="about-me hero min-h-screen bg-base-100 dark:bg-neutral dark:text-neutral-content">
 	<div class="hero-content max-w-4xl w-full flex justify-start">
 		<div class="max-w-lg">
-			<h1 class="text-neutral text-7xl md:text-8xl font-bold">About Me</h1>
+			<h1 class="text-neutral text-7xl md:text-8xl font-bold dark:text-neutral-content">About Me</h1>
 			<p class="text-lg md:text-xl max-w-md">
 				I am a self-taught programmer from PH, currently learning Go for the backend and Svelte for
 				the frontend, and I also have an interest in learning Rust.
 			</p>
 			<div class="social mt-2">
-				<a target="_blank" rel="noopener" class="badge badge-accent badge-lg" href="https://github.com/mystique09">Github</a>
-				<a target="_blank" rel="noopener" class="badge badge-accent badge-lg" href="https://www.linkedin.com/in/benjie-ben-garcia-916261202">LinkedIn</a>
+				<a id="github-aboutme" aria-label="github-aboutme" target="_blank" rel="noopener" class="badge badge-accent badge-lg dark:badge-secondary" href="https://github.com/mystique09">Github</a>
+				<a id="linkedin-aboutme" aria-label="linkedin-aboutme" target="_blank" rel="noopener" class="badge badge-accent badge-lg dark:badge-secondary" href="https://www.linkedin.com/in/benjie-ben-garcia-916261202">LinkedIn</a>
 			</div>
 		</div>
 	</div>
