bf1f8af3488f3f5c7efba3223160f2aedc08f4c6
mystique09 <knightkrypto7@gmail.com>
2022-11-21 03:19:25 +0800

    fix: duplicate id

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -51,27 +51,26 @@
 {#if showNav}
 <div transition:fly={{y: 200, duration: 500}} class="fixed top-0 right-0 h-screen w-18 z-10 flex items-center mr-4">
 	<div class="flex flex-col items-center justify-center h-auto bg-neutral py-4 px-1 rounded-full gap-1">
-		<a id="home" aria-label="home" href="/" class="btn btn-ghost btn-circle">
+		<a aria-label="home" id="home" href="/" class="btn btn-ghost btn-circle">
 			<div>
 				<svg class="w-6 h-6 stroke-primary dark:stroke-secondary" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
 					<path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
 				  </svg>				  
 			</div>
 		</a>
-		<a id="about-me" aria-label="about-me" href="/#about-me" class="btn btn-ghost btn-circle">
+		<a aria-label="about-me" href="/#about-me" class="btn btn-ghost btn-circle">
 			<div>
 				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 stroke-primary dark:stroke-secondary">
 					<path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
 				  </svg>				  
 			</div>
 		</a>
-		<a id="projects" aria-label="projects" href="/#projects" class="btn btn-ghost btn-circle">
+		<a aria-label="projects" href="/#projects" class="btn btn-ghost btn-circle">
 			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 stroke-primary dark:stroke-secondary">
 			<path stroke-linecap="round" stroke-linejoin="round" d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 01-1.125-1.125v-3.75zM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-8.25zM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-2.25z" />
 		  </svg>
-		  
 		</a>
-		<a id="skills" aria-label="skills" href="/#skills" class="btn btn-ghost btn-circle">
+		<a aria-label="skills" href="/#skills" class="btn btn-ghost btn-circle">
 			<div>
 				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 stroke-primary dark:stroke-secondary">
 					<path stroke-linecap="round" stroke-linejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z" />
