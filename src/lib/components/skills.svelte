0491db8b0e3693560626e43a8c6bb747586f25fc
mystique09 <knightkrypto7@gmail.com>
2022-12-24 11:17:31 +0800

    feat: center when mobile

--- src/lib/components/skills.svelte
+++ src/lib/components/skills.svelte
@@ -11,21 +11,31 @@
 	import Tailwind from './skills/tailwind.svelte';
 </script>
 
-<div id="skills" class="about-me hero min-h-screen bg-base-100 dark:bg-neutral dark:text-neutral-content">
+<div
+	id="skills"
+	class="about-me hero min-h-screen bg-base-100 dark:bg-neutral dark:text-neutral-content"
+>
 	<div class="hero-content max-w-4xl w-full flex justify-start">
 		<div class="w-full flex flex-col md:flex-row items-start md:items-center justify-between gap-8">
 			<div>
-				<h1 class="text-neutral text-7xl md:text-8xl font-bold dark:text-neutral-content">Skills</h1>
-				<p class="mt-2 text-base tracking-wide md:text-lg max-w-lg">The programming languages, tech stacks, and tools listed below are things that I have learned and mastered over the years of my self-guided learning journey. I am constantly seeking out new opportunities to learn and improve my skills, and I am proud of the diverse range of technologies that I have mastered.				</p>
+				<h1 class="text-neutral text-7xl md:text-8xl font-bold dark:text-neutral-content">
+					Skills
+				</h1>
+				<p class="mt-2 text-base tracking-wide md:text-lg max-w-lg">
+					The programming languages, tech stacks, and tools <span class="md:hidden"
+						>listed below</span
+					> are things that I have learned and mastered over the years of my self-guided learning journey.
+					I am constantly seeking out new opportunities to learn and improve my skills, and I am proud
+					of the diverse range of technologies that I have mastered.
+				</p>
 			</div>
-			<div class="grid grid-cols-4 grid-rows-3 skills mt-4 gap-4">
+			<div class="grid grid-cols-4 grid-rows-3 self-center skills mt-4 gap-4">
 				<Css />
 				<ExpressJs />
 				<Go />
 				<Html />
 				<Javascript />
 				<NodeJs />
-				<Rust />
 				<Sass />
 				<Svelte />
 				<Tailwind />
