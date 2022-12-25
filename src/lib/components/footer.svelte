dc1d2803e967c9a347b8f1f65960e368d2162e0d
mystique09 <knightkrypto7@gmail.com>
2022-12-26 00:00:00 +0800

    feat: set footer max-width

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -3,20 +3,36 @@
 	import LogoLinkedin16 from 'carbon-icons-svelte/lib/LogoLinkedin16';
 </script>
 
-<footer class="footer p-10 bg-neutral text-neutral-content dark:bg-base-100 dark:text-base-content">
-	<div>
-		<img width="50" height="50" src="/favicon.png" alt="footer avatar" />
-		<p>Benjie Ben Garcia. <br>©	{new Date().getFullYear()}</p>
-	</div>
-	<div class="div">
-		<span class="text-neutral-content font-bold dark:text-base-content uppercase mb-0.5">Social</span>
-		<div class="grid grid-flow-col gap-4">
-			<a id="github" aria-label="github" target="_blank" rel="noreferrer" href="https://github.com/mystique09">
-				<Github />
-			</a>
-			<a id="linkedin" aria-label="linkedin" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/benjie-ben-garcia-916261202">
-				<LogoLinkedin16 />
-			</a>
+<footer class="w-full p-10 bg-neutral text-neutral-content dark:bg-base-100 dark:text-base-content">
+	<div class="footer w-full max-w-4xl m-auto">
+		<div>
+			<img width="50" height="50" src="/favicon.png" alt="footer avatar" />
+			<p>Benjie Ben Garcia. <br />© {new Date().getFullYear()}</p>
+		</div>
+		<div class="div">
+			<span class="text-neutral-content font-bold dark:text-base-content uppercase mb-0.5"
+				>Social</span
+			>
+			<div class="grid grid-flow-col gap-4">
+				<a
+					id="github"
+					aria-label="github"
+					target="_blank"
+					rel="noreferrer"
+					href="https://github.com/mystique09"
+				>
+					<Github />
+				</a>
+				<a
+					id="linkedin"
+					aria-label="linkedin"
+					target="_blank"
+					rel="noreferrer"
+					href="https://www.linkedin.com/in/benjie-ben-garcia-916261202"
+				>
+					<LogoLinkedin16 />
+				</a>
+			</div>
 		</div>
 	</div>
 </footer>
