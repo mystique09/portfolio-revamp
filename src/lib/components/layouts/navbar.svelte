71e39a1bebed029c79f075eaba3aaac038de7a53
mystique09 <knightkrypto7@gmail.com>
2023-08-07 21:25:46 +0800

    add: initial navbar layout

--- src/lib/components/layouts/navbar.svelte
+++ src/lib/components/layouts/navbar.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import { fade, fly } from 'svelte/transition';
 	import { onMount } from 'svelte';
+	import { css } from 'styled-system/css';
+	import { circle, container, hstack } from 'styled-system/patterns';
+	import { Github, GithubIcon, LogOut, MenuIcon } from 'lucide-svelte';
 
 	let showNav: boolean = false;
 
@@ -49,169 +52,74 @@
 	});
 </script>
 
-{#if showNav}
-	<div
-		transition:fly={{ y: 200, duration: 500 }}
-		class="fixed top-0 right-0 h-screen w-18 z-10 flex items-center mr-4"
+<heading
+	class={css({
+		w: 'full',
+		h: 'auto',
+		pos: 'sticky',
+		top: '0'
+	})}
+>
+	<nav
+		class={container({
+			py: '4',
+			bg: 'black',
+			opacity: '90%',
+			blur: 'md',
+			color: 'white',
+			borderBottom: '1px solid black'
+		})}
 	>
 		<div
-			class="flex flex-col items-center justify-center h-auto bg-neutral py-4 px-1 rounded-full gap-1"
+			class={hstack({
+				w: 'full',
+				h: 'full',
+				justify: 'space-between'
+			})}
 		>
-			<a aria-label="home" id="home" href="/" class="btn btn-ghost btn-circle">
-				<div>
-					<svg
-						class="w-6 h-6 stroke-primary dark:stroke-secondary"
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-						/>
-					</svg>
-				</div>
-			</a>
-			<a aria-label="about-me" href="/#about-me" class="btn btn-ghost btn-circle">
-				<div>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="w-6 h-6 stroke-primary dark:stroke-secondary"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-						/>
-					</svg>
-				</div>
-			</a>
-			<a aria-label="skills" href="/#skills" class="btn btn-ghost btn-circle">
-				<div>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="w-6 h-6 stroke-primary dark:stroke-secondary"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z"
-						/>
-					</svg>
-				</div>
-			</a>
-			<a aria-label="projects" href="/#projects" class="btn btn-ghost btn-circle">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="w-6 h-6 stroke-primary dark:stroke-secondary"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 01-1.125-1.125v-3.75zM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-8.25zM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-2.25z"
-					/>
-				</svg>
-			</a>
-			<button
-				aria-label="theme-toggle"
-				id="theme-toggle"
-				name="theme-toggle"
-				on:click={() => toggleTheme()}
-				type="button"
-				class="btn btn-ghost btn-circle"
+			<a
+				class={hstack({
+					fontSize: 'xl',
+					fontWeight: '700'
+				})}
+				href="/"
 			>
-				<div>
-					{#if currentTheme === 'light'}
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							class="w-6 h-6 stroke-primary dark:stroke-secondary"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
-							/>
-						</svg>
-					{:else}
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							class="w-6 h-6 stroke-primary dark:stroke-secondary"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
-							/>
-						</svg>
-					{/if}
+				<div class={css({ w: '8', h: '8', pos: 'relative' })}>
+					<img
+						class={css({ pos: 'absolute', h: 'full', w: 'full' })}
+						src="/favicon.webp"
+						alt="Skull"
+					/>
 				</div>
+				<span class={css({ fontSize: 'md' })}>BNJBN.G</span>
+			</a>
+			<ul
+				class={hstack({
+					hideBelow: 'sm'
+				})}
+			>
+				<li>
+					<a href="/#about">About</a>
+				</li>
+				<li>
+					<a href="/#skills">Skills</a>
+				</li>
+				<li>
+					<a href="/#projects">Projects</a>
+				</li>
+				<li>
+					<a href="/#socials">Socials</a>
+				</li>
+			</ul>
+			<button
+				class={css({
+					hideFrom: 'md',
+					color: 'white'
+				})}
+				type="button"
+			>
+				<MenuIcon />
 			</button>
 		</div>
-	</div>
-{/if}
-
-<div class="fixed bottom-0 right-0 h-24 z-10 flex flex-row items-center px-4">
-	<div class="w-full flex items-center justify-end">
-		<button
-			aria-label="settings"
-			type="button"
-			id="settings"
-			name="settings"
-			on:click={() => (showNav = !showNav)}
-			class="btn btn-neutral btn-circle"
-		>
-			{#if !showNav}
-				<svg
-					in:fade={{ duration: 300 }}
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="w-6 h-6 stroke-primary dark:stroke-secondary"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z"
-					/>
-				</svg>
-			{:else}
-				<svg
-					in:fade={{ duration: 300 }}
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="w-6 h-6 stroke-primary dark:stroke-secondary"
-				>
-					<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-				</svg>
-			{/if}
-		</button>
-	</div>
-</div>
+	</nav>
+</heading>
