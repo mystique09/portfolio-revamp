e01bb6f1b77581d3688493f6d88a8afe374cfb49
mystique09 <knightkrypto7@gmail.com>
2023-08-18 21:30:31 +0800

    feat: basic layout and content

--- src/lib/components/Homepage/hero.svelte
+++ src/lib/components/Homepage/hero.svelte
@@ -1,31 +1,40 @@
 <script lang="ts">
 	import { css } from 'styled-system/css';
-	import { container, float, vstack } from 'styled-system/patterns';
+	import { container, float, vstack, wrap } from 'styled-system/patterns';
 
-	let skills: string[] = [
-		'go developer',
-		'typescript',
-		'proficient in javascript',
-		'a backend developer',
-		'self-taught'
+	let skillSet: string[] = [
+		'am Back-end developer.',
+		'am a self-taught developer.',
+		'am a Golang fan boi.',
+		'know a little bit about Front-end development.'
 	];
 	let i = 0;
 
-	setInterval(() => (i = (i + 1) % skills.length), 3000);
+	setInterval(() => (i = (i + 1) % skillSet.length), 3000);
 
-	$: currentSkill = skills[i];
+	$: currentSkill = skillSet[i];
 </script>
 
-<main id="#" class={css({ bgColor: 'neutral.950', h: '100vh' })}>
+<main id="#" class={css({ bgColor: 'neutral.950', h: 'screen' })}>
 	<div class={container()}>
-		<div class={css({ pos: 'relative', py: '2rem' })}>
-			<!--
+		<span
+			class={float({
+				placement: 'middle-center',
+				color: 'neutral.900',
+				opacity: '60%',
+				fontSize: '40rem',
+				zIndex: '1'
+			})}
+		>
+			B
+		</span>
+		<div class={css({ pos: 'relative', py: '2rem', zIndex: '2' })}>
 			<h1
-				class={css({
-					fontSize: '3xl',
+				class={wrap({
+					fontSize: '5xl',
 					fontWeight: 'bold',
 					color: 'white',
-					lineHeight: '1.5rem',
+					lineHeight: '3.5rem',
 					mb: '2rem',
 					md: {
 						fontSize: '3xl'
@@ -35,32 +44,25 @@
 					}
 				})}
 			>
-				Benjie Ben G.
+				Hello, you can call me <span class={css({ bg: 'yellow.500', rounded: 'lg' })}
+					>{'{{'} Benjie {'}}'}</span
+				>
 			</h1>
-			-->
-			<h1
-				class={float({
-					placement: 'middle-center',
-					color: 'neutral.900',
-					fontSize: '40rem',
-					zIndex: '-1'
-				})}
-			>
-				B
-			</h1>
-
 			<div
 				class={vstack({
-					fontSize: 'sm',
+					pos: 'relative',
+					fontSize: 'lg',
 					color: 'neutral.100',
-					fontWeight: 'normal',
-					lineHeight: '1.5rem',
-					gap: "3"
+					fontWeight: 'light',
+					lineHeight: '1.7rem',
+					alignItems: 'start',
+					justifyContent: 'space-evenly',
+					gap: '3'
 				})}
 			>
 				<p>
-					Hello, welcome to my portfolio. I am excited to share with you some of my past projects
-					and accomplishments as a programmer.
+					Welcome to my portfolio. I am excited to share with you some of my past projects and
+					accomplishments as a programmer.
 				</p>
 
 				<p>
