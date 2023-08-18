eac567ef0739c90da0664c51aa15b01c39a38875
mystique09 <knightkrypto7@gmail.com>
2023-08-18 21:30:12 +0800

    add: montserrat font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
 	import Navbar from '$lib/components/layouts/navbar.svelte';
 	import Footer from '$lib/components/layouts/footer.svelte';
-	import '../app.css';
+
+	import "@fontsource/montserrat";
 	import "@fontsource/poppins"
+	import "@fontsource/poppins/100.css"
+	import "@fontsource/poppins/300.css"
+	import "@fontsource/poppins/500.css"
+	import "@fontsource/poppins/700.css"
+	
+	import '../app.css';
 </script>
 
 <Navbar />
